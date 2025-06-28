d3664de6b73ea734a60e2bf9ffb3fd5a0b8a67dd
Shuting Yang <ystfc72@gmail.com>
2025-06-28 15:53:32 +0100

    Check in the template project

---
+++ projects/.template/src/main.cpp
@@ -0,0 +1,7 @@
+//GLAD is a library that manages OpenGL function pointers
+#include <glad/glad.h> // include glad to get all the required OpenGL headers
+#include <GLFW/glfw3.h>// create an OpenGL context, define a window, and handle inputs
+#include <iostream> // for input and output operations
+int main() {
+  return 0;
+}
